01685c2fe0c745fd5e8d19f615d5d40facaeee1f
S Dheeraj <97402083+SaranaDheeraj@users.noreply.github.com>
2023-10-06 11:51:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const img = document.querySelector("img");
 const generate = document.querySelector("button");
 
 generate.addEventListener("click", () => {
-  img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text.value}`;
-  img.classList.add("img-style");
+  if (text.value === "") {
+    alert("Please Enter text or a URL!!");
+  } else {
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text.value}`;
+    img.classList.add("img-style");
+  }
 });
